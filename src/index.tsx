c502bfa286ce878c7bb84dcf70b5cb9db4d7ec14
Mykola Gataylo <mykola.gataylo@gmail.com>
2021-08-07 21:26:46 +0200

    refactor: refactor structure of code

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import "./styles.scss";
 import React from "react";
 import ReactDOM from "react-dom";
+
 import { App } from "./components/app/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
