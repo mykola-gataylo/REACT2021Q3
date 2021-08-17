4dfff8d00f2d13b45bb786af909c7411139eb8f5
Mykola Gataylo <mykola.gataylo@gmail.com>
2021-08-17 13:10:19 +0200

    refactor: refactor class-component to function-component

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,29 +1,16 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import "./_search-field.scss";
 import "../../public/img/react.png";
-import { SearchForm } from "../SearchForm/SearchForm";
+import SearchForm from "../SearchForm/SearchForm";
 
-export class SearchField extends React.Component {
-  // eslint-disable-next-line class-methods-use-this
-  renderSearchForm(): ReactNode {
-    return <SearchForm />;
-  }
+const SearchField = (): JSX.Element => {
+  return (
+    <>
+      <div className="search-field">
+        <SearchForm />
+      </div>
+    </>
+  );
+};
 
-  render(): ReactNode {
-    return (
-      <>
-        <div className="search-field">{this.renderSearchForm()}</div>
-      </>
-    );
-  }
-}
-
-// export const SearchField = (): JSX.Element => {
-//   return (
-//     <>
-//       <div className="search-field">
-//         <SearchForm />
-//       </div>
-//     </>
-//   );
-// }
+export default SearchField;
