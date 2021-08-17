5bc456c80fc5c49cce2e5e929aebe60e0604316c
Mykola Gataylo <mykola.gataylo@gmail.com>
2021-08-17 13:08:28 +0200

    refactor: refactor class-component to function-component

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,43 +1,19 @@
 /* eslint-disable react/require-render-return */
-import React, { ReactNode } from "react";
+import React from "react";
 import "./_search-form.scss";
 import "../../public/img/react.png";
-import { SearchInput } from "../SearchInput/SearchInput";
-import { SearchButton } from "../SearchButton/SearchButton";
+import SearchInput from "../SearchInput/SearchInput";
+import SearchButton from "../SearchButton/SearchButton";
 
-export class SearchForm extends React.Component {
-  // eslint-disable-next-line class-methods-use-this
-  renderSearchInput(): ReactNode {
-    return <SearchInput />;
-  }
+const SearchForm = (): JSX.Element => {
+  return (
+    <>
+      <form className="search-form" action="">
+        <SearchInput />
+        <SearchButton />
+      </form>
+    </>
+  );
+};
 
-  // eslint-disable-next-line class-methods-use-this
-  renderSearchButton(): ReactNode {
-    return <SearchButton />;
-  }
-
-  render(): ReactNode {
-    this.renderSearchInput();
-    this.renderSearchButton();
-
-    return (
-      <>
-        <form className="search-form" action="">
-          {this.renderSearchInput()}
-          {this.renderSearchButton()}
-        </form>
-      </>
-    );
-  }
-}
-
-// export const SearchForm = (): JSX.Element => {
-//     // return (
-//   <>
-//     <form className="search-form" action="">
-//       <SearchInput />
-//       <SearchButton />
-//     </form>
-//   </>
-// );
-// }
+export default SearchForm;
