58aca73e732a47439c6b3d4463337c1087d54c7b
Mykola Gataylo <mykola.gataylo@gmail.com>
2021-08-28 12:29:27 +0200

    refactor: change path for import  App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import "./styles.scss";
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./components/app/App";
+import App from "./components/App/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
